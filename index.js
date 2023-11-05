a50a5a05f433e4b73713c13601ef70ff66fc4147
Wilson E. Husin <wilson@husin.dev>
2023-11-05 12:58:43 -0800

    index: add cold-start support

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const execOpts = process.env.PLUGIN_PATH
   : null;
 
 const url = `${protocol}://${adminHost}`;
+const maxRetry = 3;
+const backoffTime = 5 * 1000; // 5 seconds
 
 const sh = (cmd) => {
   console.log(`command: ${cmd}`);
@@ -21,8 +23,69 @@ const sh = (cmd) => {
   return exec;
 };
 
+const api = new ghostAdmin({
+  url,
+  key: apiKey,
+  version: "v5.0",
+});
+
+async function logError(err) {
+  console.error("[!] Encountered error!");
+  const str = JSON.stringify(
+    {
+      ...err,
+      output: err?.output?.map((e) => e?.toString()),
+      stdout: err?.stdout?.toString(),
+      stderr: err?.stderr?.toString(),
+    },
+    null,
+    2,
+  );
+  const msg = str === "{}" ? err : str;
+  console.error(msg);
+}
+
+async function ping() {
+  try {
+    const site = await api.site.read();
+    console.log({ site });
+  } catch (err) {
+    // Log error then proceed anyway. We don't really need this info,
+    // but giving a chance to properly boot from cold start, if needed.
+    logError(err);
+    console.error("Proceeding anyway...");
+  }
+}
+
+async function upload(zipPath) {
+  const realPath = path.join(process.env.PLUGIN_PATH || ".", zipPath);
+  console.log({ url, zipPath, realPath });
+
+  let retryCount = 0;
+  while (true) {
+    try {
+      console.log(`--> Attempt ${retryCount + 1} of ${maxRetry}.`);
+      await api.themes.upload({ file: realPath });
+      break;
+    } catch (err) {
+      logError(err);
+
+      // Maximum retries has been reached.
+      if (retryCount >= maxRetry) throw new err();
+
+      console.log(`Retrying in ${backoffTime}ms...`);
+      // In the event of Ghost Admin returning 5xx from cold start,
+      // give it some chance to boot properly before retrying again.
+      await new Promise((resolve) => setTimeout(resolve, backoffTime));
+      retryCount++;
+    }
+  }
+}
+
 async function main() {
   try {
+    await ping();
+
     sh("yarn zip");
     const files = sh("ls -a dist/*.zip").toString().trim().split("\n");
     if (files.length !== 1)
@@ -32,30 +95,10 @@ async function main() {
 
     const zipPath = files[0];
 
-    console.log({ url, zipPath });
-    const api = new ghostAdmin({
-      url,
-      key: apiKey,
-      version: "v5.0",
-    });
-    await api.themes.upload({
-      file: path.join(process.env.PLUGIN_PATH || ".", zipPath),
-    });
+    await upload(zipPath);
     console.log(`${zipPath} successfully uploaded.`);
   } catch (err) {
-    console.error("Encountered error!");
-    const str = JSON.stringify(
-      {
-        ...err,
-        output: err?.output?.map((e) => e?.toString()),
-        stdout: err?.stdout?.toString(),
-        stderr: err?.stderr?.toString(),
-      },
-      null,
-      2,
-    );
-    const msg = str === "{}" ? err : str;
-    console.error(msg);
+    logError(err);
     process.exit(1);
   }
 }
